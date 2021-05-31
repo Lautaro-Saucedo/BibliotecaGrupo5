91f9952f78a4ed65d00f2219fbfdb34232b3d981
Lautaro-Saucedo <laucha277@gmail.com>
2021-05-30 23:51:43 -0300

    creado proyecto netbeans

---
+++ src/bibliotecagrupo5/BibliotecaGrupo5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibliotecagrupo5;
+
+/**
+ *
+ * @author Laucha
+ */
+public class BibliotecaGrupo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
