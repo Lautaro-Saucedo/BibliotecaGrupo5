4c423a2d64b06072a010a861e5cbf7dcaf3469e1
SturnioloBruno <82230989+SturnioloBruno@users.noreply.github.com>
2021-06-08 01:00:26 -0300

    lectorData y autorData
    estan si revisar, lo comito hoy, reviso mañana si hay algo para corregir o no

--- src/bibliotecagrupo5/controlador/AutorData.java
+++ src/bibliotecagrupo5/controlador/AutorData.java
@@ -1,8 +1,17 @@
 
 package bibliotecagrupo5.controlador;
 
+import bibliotecagrupo5.modelo.Autor;
 import bibliotecagrupo5.modelo.Conexion;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 
 /* @author @LXWeber Leandro Xavier Weber */
@@ -12,5 +21,111 @@ public class AutorData {
     
     public AutorData(){
         con = Conexion.getConexion();
+    }
+    
+    public void registrarAutor(Autor autor){
+        String query = "INSERT INTO autor(dni_autor, nombre_autor, apellido_autor, fecha_nacimiento, nacionalidad) VALUES (?,?,?,?,?)";
+        try{
+            
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, autor.getDni_autor());
+            ps.setString(2, autor.getNombre_autor());
+            ps.setString(3, autor.getApellido_autor());
+            ps.setDate(4, Date.valueOf(autor.getFecha_nacimiento()));
+            ps.setString(5, autor.getNacionalidad());
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Autor guardado correctamente");
+            }
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Error al guardar autor, asegurese que el dni no este repetido");         
+        }
+    }
+    
+    public List<Autor> listarAutores(){
+        List<Autor> autores = new ArrayList<>();
+        String query = "SELECT * FROM autor";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Autor a = new Autor();
+                a.setDni_autor(rs.getInt("dni_autor"));
+                a.setNombre_autor(rs.getString("nombre_autor"));
+                a.setApellido_autor(rs.getString("apellido_autor"));
+                a.setFecha_nacimiento(rs.getDate("fecha_nacimiento").toLocalDate());
+                a.setNacionalidad(rs.getString("nacionalidad"));
+                autores.add(a);
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al listar autores");
+        }
+        
+        return autores;
+    }
+    
+    public void borrarAutorFisico(int dni){
+         String query = "DELETE FROM autor WHERE dni_autor=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, dni);
+
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Borrado exitosamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "El autor que se desea borrar no existe");
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al borrar autor, el autor tiene libros publicados");
+        }
+    }
+    
+    public void actualizarAutor(Autor a) {
+        String query = "UPDATE autor SET nombre_autor=?,apellido_autor=?,fecha_nacimiento=?,nacionalidad=? WHERE dni_autor=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, a.getNombre_autor());
+            ps.setString(2, a.getApellido_autor());
+            ps.setDate(3, Date.valueOf(a.getFecha_nacimiento()));
+            ps.setString(4, a.getNacionalidad());
+            ps.setInt(5, a.getDni_autor());
+
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "El autor se modifico correctamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se actualizo el autor");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar autor");
+        }
+    }
+    
+    public Autor buscarAutor(int dni) {
+        Autor a = new Autor();
+        String query ="SELECT dni_autor, nombre_autor, apellido_autor, fecha_nacimiento, nacionalidad FROM autor WHERE dni_autor=?"; 
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, dni);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+
+                a.setDni_autor(rs.getInt("dni_autor"));
+                a.setNombre_autor(rs.getString("nombre_autor"));
+                a.setApellido_autor(rs.getString("apellido_autor"));
+                a.setFecha_nacimiento(rs.getDate("fecha_nacimiento").toLocalDate());
+                a.setNacionalidad("nacionalidad");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al buscar autor");
+
+        }
+        return a;
     }
 }
--- src/bibliotecagrupo5/controlador/LectorData.java
+++ src/bibliotecagrupo5/controlador/LectorData.java
@@ -63,25 +63,97 @@ public class LectorData {
         return lectores;
     }
     
-    public List<Lector> listarLectoresMorosos(){
-        List<Lector> deudores = new ArrayList<>();
-        String query = "no se";
-        try{
+    
+    public void borrarLectorFisico(int dni){
+         String query = "DELETE FROM lector WHERE dni_lector=?";
+        try {
             PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, dni);
+
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Borrado exitosamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "El lector que se desea borrar no existe");
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al borrar lector, el lector tiene prestamos activos");
+        }
+    }
+    
+    public void borrarLectorLogico(int dni){
+        String query = "UPDATE lector SET estado=false WHERE dni_lector=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setLong(1, dni);
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Lector deshabilitado.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe un lector con el dni pasado por parametro.");
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar estado del lector.");
+        }
+    }
+    
+    public void restaurarLector(int dni) {
+        String query = "UPDATE lector SET estado=true WHERE dni_lector=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setLong(1, dni);
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "Lector Habilitado.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe un lector con ese dni.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar estado del lector.");
+        }
+    }
+    
+    public void actualizarLector(Lector l) {
+        String query = "UPDATE lector SET nombre_lector=?,apellido_lector=?,estado=? WHERE dni_lector=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, l.getNombre_lector());
+            ps.setString(2, l.getApellido_lector());
+            ps.setBoolean(3, l.isEstado_lector());
+            ps.setInt(4, l.getDni_lector());
+
+            if (ps.executeUpdate() == 1) {
+                JOptionPane.showMessageDialog(null, "El lector se modifico correctamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se actualizo el lector");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar lector");
+        }
+    }
+    public Lector buscarLector(int dni) {
+        Lector l = new Lector();
+        String query ="SELECT dni_lector, nombre_lector, apellido_lector, estado FROM lector WHERE dni_lector=?"; 
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, dni);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                Lector l = new Lector();
+            if (rs.next()) {
+
                 l.setDni_lector(rs.getInt("dni_lector"));
                 l.setNombre_lector(rs.getString("nombre_lector"));
                 l.setApellido_lector(rs.getString("apellido_lector"));
                 l.setEstado_lector(rs.getBoolean("estado"));
-                deudores.add(l);
             }
             ps.close();
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null, "Error al listar deudores");
-        }
-        return deudores;
-    }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al buscar lector");
 
+        }
+        return l;
+    }
+    
 }
