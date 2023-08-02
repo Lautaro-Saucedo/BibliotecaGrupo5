d72e5cf26f0551d96a147e12023153eb583dd603
Lautaro-Saucedo <laucha277@gmail.com>
2023-08-02 19:08:50 -0300

    primer commit

--- src/bibliotecagrupo5/BibliotecaGrupo5.java
+++ src/bibliotecagrupo5/BibliotecaGrupo5.java
@@ -1,11 +1,7 @@
 package bibliotecagrupo5;
 
-import bibliotecagrupo5.controlador.AutorData;
-import bibliotecagrupo5.controlador.EjemplarData;
-import bibliotecagrupo5.controlador.LectorData;
-import bibliotecagrupo5.controlador.LibroData;
-import bibliotecagrupo5.controlador.MultaData;
-import bibliotecagrupo5.controlador.PrestamoData;
+import bibliotecagrupo5.controlador.UsuarioData;
+import bibliotecagrupo5.modelo.Usuario;
 
 /**
  *
@@ -16,17 +12,13 @@ public class BibliotecaGrupo5 {
 
     public static void main(String[] args) {
         
-        //Buenas profe! le recomiendo cargar de nuevo el script de la DB 
-        //la anterior tenia datos invalidos en las tablas, la nueva deberia estar limpia.
-        //dejamos la tabla de multas vacia para que pruebe generarMultas()
-        //por ultimo, los controles no son demasiado rigurosos, nuestra intencion
-        //es validar todo desde las vistas en la proxima entrega
         
-        AutorData ad = new AutorData();
-        EjemplarData ed = new EjemplarData();
-        LectorData lecd = new LectorData();
-        LibroData libd = new LibroData();
-        MultaData md = new MultaData();
-        PrestamoData pd = new PrestamoData();
+        UsuarioData ud = new UsuarioData();
+        
+        System.out.println(ud.usuariosPorArea("Fabrica"));
+        //holas
+        
+        
+        
     }
 }
